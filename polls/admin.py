5f25461c8c92479074b68b5da3ac3a6691b9072c
Edwin <edwin.mucho@gmail.com>
2017-12-29 00:00:26 +0900

    modified admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Question,Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# class ChoiceInline(admin.StackedInline):   # StackedInline 은 공간을 많이 차지함.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        # 제목             , 필드
+        ('Date information', {'fields': ['pub_date']}),
+        (None, {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    # fields = ['pub_date', 'question_text']  # 간단한 경우 해당하는 것 처럼 하면됨. 순서 정렬 가능.
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
